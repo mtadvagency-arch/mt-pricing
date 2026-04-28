7768beafa121c4e4459861fd316380351526bdea
mtadvagency-arch <mtadvagency@gmail.com>
2026-04-29 02:20:36 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-// V3: disabled cache while UI is being finalized
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))));
